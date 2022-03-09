3f5ad39adf8fa1cf7427caa3da5180e87677842b
Sotlight9 <100878980+Sotlight9@users.noreply.github.com>
2022-03-09 15:19:29 +0400

    add functionality

---
+++ main.js
@@ -0,0 +1,50 @@
+const selectEl = document.getElementById('select');
+const formEl = document.getElementById('form');
+
+const inputForExperienceDuration =
+  document.getElementById('experienceDuration');
+
+function addSkillsToSelectEl(skills) {
+  skills.forEach(function (skill) {
+    const optionEl = document.createElement('option');
+    optionEl.className = 'option';
+
+    optionEl.value = skill.id;
+    optionEl.innerText = skill.title;
+
+    selectEl.appendChild(optionEl);
+  });
+}
+
+async function fetchSkills() {
+  const url = 'https://bootcamp-2022.devtest.ge/api/skills';
+
+  const response = await fetch(url);
+  const json = await response.json();
+
+  return json;
+}
+
+async function postExperience() {
+  //TODO: post the skill && experience
+}
+
+window.addEventListener('load', async function () {
+  const skills = await fetchSkills();
+  addSkillsToSelectEl(skills);
+});
+
+formEl.addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const skillId = selectEl.value;
+  const experience = inputForExperienceDuration.value;
+  const divEl = document.getElementById('newTextarea');
+     const span = document.createElement('span');
+     span.innerText = `${skillId} years of experience: ${experience}`;
+     divEl.appendChild(span);
+
+  //TODO:
+  //   const response = await postExperience(skillId, experience);
+  //   console.log(response);
+});
